b65f8bd1198358f43360c2ffcc5df84c04fea0a5
Priyanka <priyankatilekar003@gmail.com>
2024-12-23 20:59:53 +0530

    created login and register page

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express")
-const dotenv = require("dotenv").config
+const dotenv = require("dotenv")
 const cors = require("cors")
-
+const {mongoose} = require("mongoose")
+const cookieParser = require("cookie-parser")
 const app = express()
+
+dotenv.config();
+
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173'
+// }));
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Database Connected"))
+.catch((err) => console.log("Database not Connected", err))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/', require('./routes/authRoutes'))
 
 const port = 8000
 app.listen(port, () => {
