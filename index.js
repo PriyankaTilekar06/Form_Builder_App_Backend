a12770a8eff9d25b9705061d9afdc1ff7532674e
Priyanka <priyankatilekar003@gmail.com>
2025-01-03 17:33:11 +0530

    committed

--- index.js
+++ index.js
@@ -7,32 +7,42 @@ const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 const port = process.env.PORT || 8000;
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  // 'http://localhost:5173'
-];
+// const allowedOrigins = [
+//   process.env.FRONTEND_URL,
+//   // 'http://localhost:5173'
+// ];
 
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   allowedHeaders: [
+//     "Content-Type",
+//     "Authorization",
+//     "Accept",
+//     "x-csrf-token",
+//     "X-Requested-With",
+//   ],
+//   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+// };
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: process.env.FRONTEND_URL,
   credentials: true,
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Accept",
-    "x-csrf-token",
-    "X-Requested-With",
-  ],
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-csrf-token", "X-Requested-With"],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE","OPTIONS"],
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+app.options("*", cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
