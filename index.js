ba58b848db9f39db028d34a1f413af143a76ce7e
Priyanka <priyankatilekar003@gmail.com>
2025-01-03 15:04:22 +0530

    committed

--- index.js
+++ index.js
@@ -1,74 +1,16 @@
-// const express = require("express")
-// const dotenv = require("dotenv")
-// const cors = require("cors")
-// const {mongoose} = require("mongoose")
-// const cookieParser = require("cookie-parser")
-// const app = express()
-
-// dotenv.config();
-
-// // app.use(cors({
-// //     credentials: true,
-// //     origin: 'http://localhost:5173'
-// // }));
-
-
-// // mongoose.connect(process.env.MONGO_URL)
-// // .then(() => console.log("Database Connected"))
-// // .catch((err) => console.log("Database not Connected", err))
-
-// mongoose.connect(process.env.MONGO_URL)
-//   .then(() => console.log('Database Connected'))
-//   .catch((err) => console.log('Database not connected', err));
-// const allowedOrigins = [
-//   'https://form-builder-app-frontend-iota.vercel.app'
-// ];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true, 
-//   allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-csrf-token", "X-Requested-With"],
-//   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-// };
-
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions)); 
-
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use(express.urlencoded({extended: false}))
-
-// app.use('/', require('./routes/authRoutes'))
-
-// const port = 8000
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`)
-// })
-
-
-
-
-
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const { mongoose } = require("mongoose");
-const cookieParser = require("cookie-parser");
-const app = express();
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const {mongoose} = require("mongoose")
+const cookieParser = require("cookie-parser")
+const app = express()
+const port = 8000
+const authRoutes = require('./routes/authRoutes')
 dotenv.config();
 
-// const allowedOrigins = ['https://form-builder-app-frontend-two.vercel.app']
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log('Database Connected'))
-  .catch((err) => console.log('Database not connected', err));
 const allowedOrigins = [
-  'https://form-builder-app-frontend-two.vercel.app'
+process.env.FRONTEND_URL
+// 'http://localhost:5173'
 ];
 
 const corsOptions = {
@@ -87,13 +29,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); 
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
 
-app.use('/', require('./routes/authRoutes'));
+app.use('/', authRoutes)
 
-const port = 8000;
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.log('Database not connected', err));
+
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
+
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,42 +1,15 @@
-// const express = require("express")
-// const router = express.Router()
-// const cors = require('cors')
-// const { test, registerUser, loginUser, getProfile } = require('../controllers/authController')
+const express = require("express")
+const router = express.Router()
+const { test, registerUser, loginUser, getProfile } = require('../controllers/authController')
 
-// router.use(
-//     cors({
-//         credentials: true,
-//         // origin: 'http://localhost:5173'
-//         origin: 'https://form-builder-app-frontend-iota.vercel.app'
-//     })
-// )
+router.get('/', test)
+router.post('/register', registerUser )
+router.post('/login', loginUser)
+router.get('/profile', getProfile)
 
-// router.get('/', test)
-// router.post('/register', registerUser )
-// router.post('/login', loginUser)
-// router.get('/profile', getProfile)
-
-// module.exports = router
+module.exports = router
 
 
 
 
-const express = require("express");
-const router = express.Router();
-const cors = require('cors');
-const { test, registerUser, loginUser, getProfile } = require('../controllers/authController');
 
-router.use(
-    cors({
-        credentials: true,
-        origin: 'https://form-builder-app-frontend-two.vercel.app'
-        // origin: 'http://localhost:5173'
-    })
-);
-
-router.get('/', test);
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/profile', getProfile);
-
-module.exports = router;
