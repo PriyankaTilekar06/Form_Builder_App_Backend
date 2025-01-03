f7046cbf7b9f587a736ad5099a0d1e2e3f6e94e4
Priyanka <priyankatilekar003@gmail.com>
2025-01-03 06:02:50 +0530

    committed

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { mongoose } = require("mongoose");
 const cookieParser = require("cookie-parser");
-
+const app = express();
 dotenv.config();
 
-const app = express();
+
 
 // const allowedOrigins = [
 //     'https://form-builder-app-frontend-iota.vercel.app'
@@ -72,29 +72,46 @@ const app = express();
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Database Connected'))
   .catch((err) => console.log('Database not connected', err));
-const allowedOrigins = [
-  'https://form-builder-app-frontend-two.vercel.app'
-];
+// const allowedOrigins = [
+//   'https://form-builder-app-frontend-two.vercel.app'
+// ];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true, 
+//   allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-csrf-token", "X-Requested-With"],
+//   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+// };
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions)); 
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+
+const allowedOrigins = ['https://form-builder-app-frontend-two.vercel.app'];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, 
-  allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-csrf-token", "X-Requested-With"],
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Allow cookies and credentials
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
 
 app.use('/', require('./routes/authRoutes'));
 
